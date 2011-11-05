3a49b700342f27c9332c6b359709c92c5cff1530
Dean Thomas <dean@spikedsoftware.com>
2011-11-05 12:25:12 +0000

    Added the BusinessLogic project Added nunit, so i can write unit tests for the implementations.

---
+++ src/DotNetORMOverview/Blog.BusinessLogic/DataAccess/Interfaces/IUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blog.BusinessLogic.DataAccess.Interfaces
+{
+    public interface IUser
+    {
+        Model.User GetUserByUsername(string username);
+    }
+}
---
+++ src/DotNetORMOverview/Blog.BusinessLogic/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blog.BusinessLogic.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// Username of the user
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Password of the user
+        /// </summary>
+        public string Password { get; set; }
+        
+        /// <summary>
+        /// Email Address of the User
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
